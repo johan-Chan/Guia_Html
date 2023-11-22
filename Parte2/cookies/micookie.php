c530e806cf992c9487488afb8ada534713e55a89
Johan Pinzon <johansantyagopinzon88@gmail.com>
2023-11-22 18:14:22 -0500

    Primera cookie

---
+++ Parte2/cookies/micookie.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leer cookie</title>
+</head>
+<body>
+    <?php
+    
+   if(isset($_COOKIE["micookie"]))
+   {
+        echo $_COOKIE["micookie"] . "<br>";
+   }else
+        {
+            echo "USTED NO HA CREADO NINGUNA COOKIE" . "<br>";
+        }
+    
+    ?>
+</body>
+</html>
