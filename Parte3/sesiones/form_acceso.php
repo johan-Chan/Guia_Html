f4ef58c644f4e1247e08f23c8f43d402f243e275
Johan Pinzon <johansantyagopinzon88@gmail.com>
2023-11-23 09:11:24 -0500

    Creacion y Diseño login

---
+++ Parte3/sesiones/form_acceso.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css?ver=1.7">
+    <link rel="icon" href="../icon/icon2.png">
+    <title>Login</title>
+</head>
+<body>
+    <form action="">
+
+        <div class="cont">
+        <p class="texto" >ACCESO AL SISTEMA</p>
+        <p class="txt" >Usuario</p>
+        <input class="cajita" type="email">
+        <p class="txt" >Contraseña</p>
+        <input class="cajita" type="password"><br><br>
+        <input class="guardar" type="submit">
+        </div>
+
+
+
+
+
+    </form>
+    
+</body>
+</html>
---
+++ Parte3/sesiones/pag_inicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/paginas_idioma.css?ver=1.4">
+    <link rel="icon" href="../icon/icon2.png">
+    <title>Español</title>
+</head>
+<body>
+    <p class="titulo">Como utilizar php</p><br><br>
+    
+    <img class="imgs" src="../icon/phppp.jpeg" width="500" height="300">
+
+    <p class="subtitulo">¿Cómo se aplica el PHP?</p>
+
+    <p class="texto"> 
+        El código PHP se integra en una página HTML, entre los símbolos de resultado . <br>Los comandos PHP 
+        irán dentro de las áreas PHP (por regla general con un punto y coma). También podrás colocar tus
+        páginas web dentro de un Editor HTML gráfico e introducir los códigos en modo HTML dentro de un 
+        editor de texto. <br>
+        PHP es un lenguaje de script múltiple, que a diferencia de por ejemplo Javascript, es ejecutado en 
+        el servidor y por tanto no permanece bajo el control del navegador. Frecuentemente los campos de 
+        entrada están en otros foros, formularios, sistema de redacción, libro de visitas y máquinas de 
+        búsqueda
+    </p>
+</body>
+</html>
