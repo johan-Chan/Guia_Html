593418194b32a5731269758a5e15702da6078fbc
Johan Pinzon <johansantyagopinzon88@gmail.com>
2023-11-23 11:26:15 -0500

    menu de producto y paag inicio

--- Parte3/Menu/menu.php
+++ Parte3/Menu/menu.php
@@ -4,10 +4,6 @@ session_start();
 if(!isset($_SESSION["usuario"]))
 {
     header("Location:form_acceso.php");
-    
-}else
-{
-    echo "BIENVENIDO AL SISTEMA " . $_SESSION["usuario"];
 }
 
 ?>
@@ -16,11 +12,22 @@ if(!isset($_SESSION["usuario"]))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/menu.css?ver=1.1">
+    <link rel="stylesheet" href="../css/menu.css?ver=1.2">
     <link rel="icon" href="../icon/icon2.png">
     <title>Menu</title>
 </head>
 <body>
+    <div class="cont">
+        <p class="titulo">Menu</p>
+        <button class="productos"><a href="">Productos</a></button><br><br>
+        <button class="pag_idioma"><a href="">Pag Idiomas</a></button>
+        
+    
+
+
+
+
+    </div>
     
 </body>
 </html>
